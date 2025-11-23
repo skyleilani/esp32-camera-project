06a0c19366f7fcf97858a666639a97224a9c4f36
Robin <entropysendearing@gmail.com>
2025-11-22 17:20:29 -0800

    clean whitespace, begin refactor of system_logger

--- components/system_logger/system_logger.c
+++ components/system_logger/system_logger.c
@@ -1,29 +1,7 @@
-#include "system_logger.h"
-
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <esp_log.h>
+#include "system_logger.h"  
 
-void system_logger_log_message(const char *format, ...)
-{
-    uint8_t error_flags; // TO_DO: Not yet integrated
-    uint32_t timestamp; // TO_DO: Not yet integrated
-    
-    va_list args;
-    va_start(args, format);
-
-    vprintf(format, args);
-    va_end(args);
-}
-
-void system_log_info(const char, *format, ...){
-    return;
-}
-
-void system_log_warn(const char, *format, ...){
-    return;
-}
-
-void system_log_error(const char, *format, ...){
-    return;
-}
+static const char *TAG = "SYSTEM_LOGGER";
