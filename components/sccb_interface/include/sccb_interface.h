d3906978cedcf011b9ca17180aff40f8859d0843
Robin <entropysendearing@gmail.com>
2026-04-01 13:54:39 -0700

    Define macros for SCCB interface configuration parameters

--- components/sccb_interface/include/sccb_interface.h
+++ components/sccb_interface/include/sccb_interface.h
@@ -1,9 +1,13 @@
 #ifndef SCCB_INTERFACE_H
 #define SCCB_INTERFACE_H
-#include "esp_err.h"
 
-esp_err_t sccb_interface_init(void);
+// SCCB_Interface config params
+#define SCCB_I2C_PORT I2C_NUM0
+#define SCCB_SCL_GPIO 22
+#define SCCB_SDA_GPIO 21
+#define SCCB_FREQ_HZ 100000
+#define SCCB_DEVICE_ADDR_WRITE 0x30 // OV2640 SCCB write addy
+#define SCCB_DEVICE_ADDR_READ 0x31 // OV2640 SCCB read addy
+#define SCCB_TIMEOUT_MS 100
 
-void sccb_interface_scan(void);
-
-#endif // SCCB_INTERFACE_H
+#endif
